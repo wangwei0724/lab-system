98acd478237c5b71d59e0faaad99b5e429c2bb9f
wangwei <wangwei@dataguiding.com>
2017-11-02 09:21:50 +0800

    map

---
+++ public/js/my.js
@@ -0,0 +1,6 @@
+$(function(){
+	//var clientX = window.screen.availWidth;
+	var clientY = document.documentElement.clientHeight;
+	document.body.style.height = clientY + "px";
+	//document.body.style.height = availHeight+"px";
+})
